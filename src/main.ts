002f66b8349f07c7ac8c4771c0a7f21f966e1428
Hizkia <2381016@unai.edu>
2025-05-01 23:18:08 +0700

    backend

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:5173', // ganti dengan domain frontend kamu
+    origin: 'https://layanan-pengaduan-fe.vercel.app/', // ganti dengan domain frontend kamu
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
